8db387c1bc145239304409faa017fed9b22f76d5
zzz845zz <zzz845zz@gmail.com>
2020-06-07 01:45:08 +0900

    0607

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -7,7 +7,38 @@ def get_specific_dong(df, dongname='중앙동'):
     
     return df[is_matched]
 
-def split_traintest(df, criteria='연월', threshold=202000):
+def split_train_val_test_by_file(dict_df={}, trainfiles=[], validatefiles=[], testfiles=[], category=None):
+    train = pd.DataFrame()
+    validate = pd.DataFrame()
+    test = pd.DataFrame()
+
+    for f in trainfiles:
+        target = dict_df[f] if category==None else dict_df[f][dict_df[f]['서비스_업종_코드_명']==category]
+        
+        prev_shape = train.shape
+        train = train.append(target)
+        print('[train] %s : %s, accumulate : %s' %(f, target.shape, train.shape))
+        assert (prev_shape[0] + target.shape[0]) == train.shape[0]
+    
+    for f in validatefiles:
+        target = dict_df[f] if category==None else dict_df[f][dict_df[f]['서비스_업종_코드_명']==category]
+        
+        prev_shape = validate.shape
+        validate = validate.append(target)
+        print('[validate] %s : %s, accumulate : %s' %(f, target.shape, validate.shape))
+        assert (prev_shape[0] + target.shape[0]) == validate.shape[0]
+        
+    for f in testfiles:
+        target = dict_df[f] if category==None else dict_df[f][dict_df[f]['서비스_업종_코드_명']==category]
+        
+        prev_shape = test.shape
+        test = test.append(target)
+        print('[test] %s : %s, accumulate : %s' %(f, target.shape, test.shape))
+        assert (prev_shape[0] + target.shape[0]) == test.shape[0]
+        
+    return train, validate, test
+
+def split_traintest_by_column(df, criteria='연월', threshold=202000):
     is_train = df[criteria] < threshold
     is_test = df[criteria] >= threshold
     
@@ -16,12 +47,9 @@ def split_traintest(df, criteria='연월', threshold=202000):
     
     return train, test
 
-def split_xy(df, x_header=None, y_header=None, include_region=False):
-    is_valid = df[y_header] != 0
-    df = df[is_valid]
-    
-    x_header = [x for x in df.columns if x[0]=='남' or x[0]=='여' or x=='연월'] if x_header==None else x_header
-    y_header = y_header if y_header==None else y_header
+def split_xy(df, x_header=None, y_header=None, next_quarter=False):
+    # is_valid = df[y_header] != 0
+    # df = df[is_valid]
     
     x = np.array(df[x_header])
     y = np.array(df[y_header])
--- utils/regression.py
+++ utils/regression.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LinearRegression, ElasticNet
 from sklearn.svm import SVR
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.neural_network import MLPRegressor
 
 def get_model_LinearRegression(x_train, y_train, normalize=False):
     model = LinearRegression(normalize=normalize)
@@ -21,3 +21,17 @@ def get_model_SVR(x_train, y_train):
     model = make_pipeline(SVR(C=1.0, epsilon=0.2))
     model.fit(x_train, y_train)
     return model
+
+
+def get_model_MLPRegression(x_train, y_train, hidden_layers, max_iter, alpha, random_state):
+    model = MLPRegressor(
+        hidden_layer_sizes=hidden_layers,
+        max_iter = max_iter,
+        alpha = alpha,
+        random_state = random_state
+    )
+    model.fit(x_train, y_train)
+    return model
+    
+    
+    
